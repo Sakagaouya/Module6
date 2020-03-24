c1b60d874d1bffe8d846a4f73b20c4c659e1ef96
Sakagaouya <sara.denoual2018@campus-eni.fr>
2020-03-24 16:46:03 +0100

    Ajoutez des fichiers projet.

---
+++ TPDojo1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TPDojo1.MvcApplication" Language="C#" %>
---
+++ TPDojo1/Models/DojoViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TPDojo1_BO;
+
+namespace TPDojo1.Models
+{
+    public class DojoViewModel
+    {
+        public Samourai Samourai { get; set; }
+        public List<Arme> Armes { get; set; }
+        public int? IdSelectedArme { get; set; }
+    }
+}
